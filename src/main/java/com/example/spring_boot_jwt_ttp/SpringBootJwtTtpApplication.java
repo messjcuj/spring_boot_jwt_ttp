0037744de3393456a26ddb54756c941ccd0fa9d2
Your Name <tuanphuc2k@gmail.com>
2021-09-26 15:16:04 +0700

    master

---
+++ src/main/java/com/example/spring_boot_jwt_ttp/SpringBootJwtTtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_ttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtTtpApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/spring_boot_jwt_ttp/entity/User.java
@@ -0,0 +1,43 @@
+package com.example.spring_boot_jwt_ttp.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
